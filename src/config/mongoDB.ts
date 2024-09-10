c501fe81a82eeb7772804a1f48a9cd8573883d59
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-09-10 14:08:14 +0300

    feat: Added mongoDB script to connect to database

---
+++ src/config/mongoDB.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async (): Promise<void> => {
+
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/productAPI",);
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
+    }
+    catch(error) {
+        console.error(`MongoDB Connection Error: ${(error as Error).message}`);
+        process.exit(1);
+    }
+}
+
+export default connectToMongoDB;
