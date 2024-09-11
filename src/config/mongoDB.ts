6ce5a11b273c1cba0766c0e5c68bcb7702ed5b5e
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-09-11 14:50:29 +0300

    refactor: Added retry mechanism to mongoDB connection

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async (): Promise<void> => {
+    const maxRetries = 5;
+    const retryInterval = 5000;
+    let retries = 0;
+
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/productAPI",);
+
         console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
-        console.error(`MongoDB Connection Error: ${(error as Error).message}`);
-        process.exit(1);
+        while (retries < maxRetries) {
+            retries++;
+            console.error(`MongoDB Connection Error: ${(error as Error).message}`);
+
+            console.log(`Retrying to connect... (${retries}/${maxRetries})`);
+            await new Promise(resolve => setTimeout(resolve, retryInterval));
+        }
     }
 }
 
