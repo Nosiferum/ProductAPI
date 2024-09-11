06dbc6bfed44cc79bffe95f76f55210a22a9d7bd
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-09-11 13:21:40 +0300

    style: Used object destructuring for imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import {Router, Request, Response} from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+router.get('/', (req: Request, res: Response) => {
     res.send('Welcome');
 });
 
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import {Request, Response, Router} from "express";
 import Product from "../models/product";
 import mongoose from "mongoose";
 import wrapAsync from "../utils/wrapAsync";
 
-const router = express.Router();
+const router = Router();
 
-router.get("/", wrapAsync(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+router.get("/", wrapAsync(async (req: Request, res: Response) => {
     const foundProducts = await Product.find({});
 
     if (!foundProducts) {
@@ -15,7 +15,7 @@ router.get("/", wrapAsync(async (req: express.Request, res: express.Response, ne
     res.status(200).json(foundProducts);
 }))
 
-router.get("/:id", wrapAsync(async (req: express.Request, res: express.Response) => {
+router.get("/:id", wrapAsync(async (req: Request, res: Response) => {
     const {id} = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -31,12 +31,12 @@ router.get("/:id", wrapAsync(async (req: express.Request, res: express.Response)
     res.status(200).json(foundProduct);
 }))
 
-router.post("/", wrapAsync(async (req: express.Request, res: express.Response) => {
+router.post("/", wrapAsync(async (req: Request, res: Response) => {
     await Product.create(req.body);
     res.status(201).send('Product Added Successfully');
 }))
 
-router.put("/:id", wrapAsync(async (req: express.Request, res: express.Response) => {
+router.put("/:id", wrapAsync(async (req: Request, res: Response) => {
     const {id} = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -52,7 +52,7 @@ router.put("/:id", wrapAsync(async (req: express.Request, res: express.Response)
     res.status(200).json(foundProduct);
 }))
 
-router.delete("/:id", wrapAsync(async (req: express.Request, res: express.Response) => {
+router.delete("/:id", wrapAsync(async (req: Request, res: Response) => {
     const {id} = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -63,6 +63,7 @@ router.delete("/:id", wrapAsync(async (req: express.Request, res: express.Respon
 
     if (!deletedProduct) {
         res.status(404).json({error: "Product not found with given ID"});
+        return;
     }
     res.status(204).send('Product Removed Successfully');
 }))
